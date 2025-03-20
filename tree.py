aa62163d2007d173a68d0d13707f5778a6514334
jwheller0013 <jwheller0013@yahoo.com>
2025-03-20 15:19:21 -0400

    Working product.

--- tree.py
+++ tree.py
@@ -38,13 +38,19 @@ cwd = os.getcwd()
 
 def list_dir (start, alist):
     dircontents = os.listdir(os.path.realpath(start))
+    with open('test_file3', 'a') as f:
+        f.write(str(os.path.realpath(start)) + "\n")
+        f.close()
     for i in dircontents:
-        with open('test_file2', 'a') as f:
-            f.write(str(i) + "\n")
-            f.close()
+        # with open('test_file2', 'a') as f:
+        #     f.write(str(i) + "\n")
+        #     f.close()
         newpath = os.path.join(start, i)
         if os.path.isfile(cwd+'/'+ str(newpath)) == True:
             alist.append(newpath)
+            with open('test_file3', 'a') as f:
+                f.write(str(i) + "\n")
+                f.close()
         elif os.path.isdir(cwd+'/'+ str(newpath)) == True:
             list_dir(newpath, alist)
 #slide 162 in thinkpython2 pdf
